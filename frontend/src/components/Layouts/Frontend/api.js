36ca356315c2a3f5cb833b4ad8de4647e15954ef
Arif <arifindex22@gmail.com>
2024-03-06 22:22:19 +0600

    Update api.js

--- frontend/src/components/Layouts/Frontend/api.js
+++ frontend/src/components/Layouts/Frontend/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', // Update with your Laravel API URL
+  //baseURL: 'http://localhost:8000/api/v1', // Update with your Laravel API URL
+  baseURL: 'https://dhroobo-admin.dhakaitsolutions.com/api/v1', // Update with your Laravel API URL
 });
 
 api.interceptors.response.use(
